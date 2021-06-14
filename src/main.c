01fa099ce50389e936ad91a1e078abad534ebe28
honbey <git@honbey.com>
2021-06-14 18:53:54 +0800

    Test for `modInversion()` and update README.md

--- src/main.c
+++ src/main.c
@@ -16,11 +16,14 @@ int main(int argc, char const *argv[]) {
   printInteger(src, 10);
   printInteger(exp, 10);
   printInteger(mod, 10);
-  Integer* r = modExponent(src, exp, mod);
+//  Integer* r = modExponent(src, exp, mod);
+  Integer* t = plusMinus(src, exp, 0);
+  Integer* r = modInversion(t, mod);
   printInteger(r, 10);
   free(src);
   free(exp);
   free(mod);
+  free(t);
   free(r);
 
   return 0;
